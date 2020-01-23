33c086c24a76f5bfbb5d767e07720d123f308464
Srinivasa Ashok Kumar <ashokslsk@gmail.com>
2020-01-23 19:32:53 +0530

    Added app bar Finished ROW and Columns

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,22 +5,109 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return  Center(
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-//        width: 200.0,
-//        height: 100.0,
-//        margin: EdgeInsets.all(15.0),
-        child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-        style: TextStyle(
-            decoration: TextDecoration.none,
-                fontSize: 75.0,
+    return  MaterialApp(
+        title: "Exploring UI Widgets",
+        home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: Container(
+                alignment: Alignment.center,
+                color: Colors.deepPurple,
+                child:
+                Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(child: Text(
+                        "India",
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
 
-        ),),
-      ),
+                      Expanded(child: Text(
+                        "Popular for IT and Agriculture",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 12.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
+                    ],
+                  ),
+
+
+                  Row(
+                    children: <Widget>[
+                      Expanded(child: Text(
+                        "China",
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
+
+                      Expanded(child: Text(
+                        "Popular for Duplication and Border breach",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 12.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
+                    ],
+                  ),
+
+                  Row(
+                    children: <Widget>[
+                      Expanded(child: Text(
+                        "SriLanka",
+                        textAlign: TextAlign.center,
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
+
+                      Expanded(child: Text(
+                        "Popular for Seafood and Agriculture",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 12.0,
+                            fontFamily: 'SixCaps',
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                        ),)),
+                    ],
+                  )
+                ],)
+            ),
+          ),
+        )
+
+
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'app_screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Exploring UI Widgets",
-    home: Home(),
-  ));
+  runApp(Home());
 }
 
